63db4d51208af62bf960a5b4ce88674281ecb01d
Elias Schneider <login@eliasschneider.com>
2025-08-24 15:30:18 +0200

    chore(migrations): add postgres down migration to 20250822000000

--- backend/resources/migrations/postgres/20250822000000_foreign_keys_improvements.down.sql
+++ backend/resources/migrations/postgres/20250822000000_foreign_keys_improvements.down.sql
@@ -1,1 +1,7 @@
--- No-op
+ALTER TABLE public.audit_logs
+    DROP CONSTRAINT IF EXISTS audit_logs_user_id_fkey,
+    ADD CONSTRAINT audit_logs_user_id_fkey
+        FOREIGN KEY (user_id) REFERENCES public.users (id);
+
+ALTER TABLE public.oidc_authorization_codes
+    DROP CONSTRAINT IF EXISTS oidc_authorization_codes_client_fk;
