cc34aca2a0c6dd174a7e7b11077bf1b62bcc7582
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2025-09-20 12:32:00 -0700

    fix: do not treat certain failures in app images bootstrap as fatal (#966)

--- backend/internal/bootstrap/application_images_bootstrap.go
+++ backend/internal/bootstrap/application_images_bootstrap.go
@@ -42,9 +42,15 @@ func initApplicationImages() error {
 		name := f.Name()
 		destFilePath := path.Join(dirPath, name)
 
+		// Skip directories
+		if f.IsDir() {
+			continue
+		}
+
 		h, err := utils.CreateSha256FileHash(destFilePath)
 		if err != nil {
-			return fmt.Errorf("failed to get hash for file '%s': %w", name, err)
+			slog.Warn("Failed to get hash for file", slog.String("name", name), slog.Any("error", err))
+			continue
 		}
 
 		// Check if the file is a legacy one - if so, delete it
